7864056f54fdc67892ea10b4bb5a524109929153
LouisRialland <Lo.Rialland@gmail.com>
2016-12-13 19:30:45 +0100

    reapply app.js

--- LuminoteBis/app.js
+++ LuminoteBis/app.js
@@ -1,1 +1,30 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs'),
+    MongoClient = require("mongodb").MongoClient;
 
+
+
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+app.get('/front', function (req, res) {
+    res.sendfile(__dirname + '/pageFront.html');
+});
+app.get('/behemoth.mp3', function (req, res) {
+    res.sendfile(__dirname + '/behemoth.mp3');
+});
+app.get('/future_club.mp3', function (req, res) {
+    res.sendfile(__dirname + '/future_club.mp3');
+});
+
+io.sockets.on('connection', function (socket) {
+    console.log('coucou');
+    socket.emit('HEY ON A RECU');
+
+});
+
+server.listen(3200);
