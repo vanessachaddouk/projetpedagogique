553b3e7f0ee58340c7c26492c7aff57307430312
LouisRialland <Lo.Rialland@gmail.com>
2016-12-13 19:57:56 +0100

    cleaning app.js

--- LuminoteBis/app.js
+++ LuminoteBis/app.js
@@ -23,43 +23,6 @@ app.get('/future_club.mp3', function (req, res) {
 io.sockets.on('connection', function (socket) {
     console.log('coucou');
     socket.emit('HEY ON A RECU');
-    MongoClient.connect("mongodb://localhost/rapsodie", function(error, db) {
-
-        db.collection("content").find({page:1}).toArray(function (error, results) {
-            if (error) throw error;
-
-            results.forEach(function(i, obj) {
-                socket.emit('creation_button', {contenu:results[obj].contenu, type:results[obj].type, object: results[obj]._id ,description : results[obj].description});
-            });
-        });
-        socket.on('envoi_donnees',function(id){
-            var MongoObjectID = require("mongodb").ObjectID;
-            var idToFind      = id;           // Identifiant, sous forme de texte
-            var objToFind     = { _id: new MongoObjectID(idToFind) }; // Objet qui va nous servir pour effectuer la recherche
-
-
-            db.collection("content").findOne(objToFind,function(error,result){
-
-                socket.broadcast.emit('data_visualisation',{type:result.type , contenu:result.contenu});
-            });
-
-        });
-
-
-        // REQUIRE button on new page
-
-        socket.on('require_button',function(page_require){
-            db.collection("content").find({page:page_require}).toArray(function (error, results) {
-                if (error) throw error;
-
-                results.forEach(function(i, obj) {
-                    socket.emit('creation_button', {contenu:results[obj].contenu, type:results[obj].type, object: results[obj]._id ,description : results[obj].description});
-                });
-            });
-        });
-    });
-
-
 
 });
 
